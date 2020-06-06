d619198685d4eccd5afde85246b17f28946825be
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-06-06 10:09:33 +0530

    Events model updated

--- models/Event.js
+++ models/Event.js
@@ -1,36 +1,41 @@
 const mongoose = require('mongoose')
-const validator = require('validator')
+const Schema = mongoose.Schema
 
-const eventSchema = new mongoose.Schema({
+const eventsSummarySchema = new Schema({
+    gallery : [],                           // will contain url of the uploaded images in simple array
+    chief_guest : {type:String},
+    award_winners : {type:String},
+    summary : {type:String},
+    outside_links : [],
+    file_attachment : [],
+    video_links : {type:String}
+})
+
+
+const eventSchema = new Schema({
     name:{
         type: String,
         required: true,
         trim: true
     },
-    venue:{
-        type:String
-    },
-    date : {
-        type: Date,
-        required : true
-    },
-    description:{
-        type: String,
-        required: true
-    },
-    poster_url:{
-        type: String,
-        required: true
-    },
+    venue:{type:String},
+    date : {type: Date,required : true},
+    description:{type: String,required: true},
+    poster_url:{type: String,required: true},
     owner:{
-        type: mongoose.Schema.Types.ObjectId,
+        type: Schema.Types.ObjectId,
         required: true,
         ref: 'User'
-    }
+    },
+    participants : [],
+    categories : {type:String,required:true},       // workshop, competiotion, talk-show
+    speaker : {type:String},
+    event_summary = {type:Schema.Types.ObjectId, ref: 'event_summary'}
 },{
     timestamps: true
 })
 
+const event_summary = mongoose.model('event_summary',eventsSummarySchema)
 const Event = mongoose.model('Events',eventSchema);
 
 module.exports = Event
