81b79dc8ab40d0fc02ce3575bd4bf31a08b3907b
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-06-06 10:29:14 +0530

    added new attribute bio to user schema

--- models/Users.js
+++ models/Users.js
@@ -5,12 +5,13 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     user_id:{type:String,required:true},
     pswd:{type:String,required:true},
-    name:{type:String,required:false},
-    contact:{type:String,required:false},
-    email_id:{type:String,required:false},
-    dp_url:{type:String,required:false},
+    name:{type:String},
+    contact:{type:String},
+    email_id:{type:String},
+    dp_url:{type:String},
     club_head:{type:Boolean,required:true},
-    club_name:{type:String,required:false}
+    club_name:{type:String},
+    bio:{type:String}
 },{
     timestamps:true
 });
