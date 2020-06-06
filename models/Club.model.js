e2e0d6d8915f9de51b9f6a932e22811d7d98f2bf
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-06-06 10:26:44 +0530

    new Club schema created

---
+++ models/Club.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');)
+const Schema = mongoose.Schema;
+
+const clubSchema = new Schema({
+    name:{type:String,required:true},
+    head:{type:String},
+    description:{type:String},
+    logo:{type:String}
+})
+
+const Club = mongoose.model('club',clubSchema)
+
+module.exports = Club
