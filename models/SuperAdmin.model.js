16948981d63a9f07acd75044c874dd71c92ac439
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-06-06 13:20:05 +0530

    new Super Admin Chema created

---
+++ models/SuperAdmin.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const superAdminSchema = new Schema({
+    user_id:{type:String,required:true},
+    pswd:{type:String,required:true},
+    name:{type:String},
+    contact:{type:String},
+    email_id:{type:String},
+    dp_url:{type:String},
+    bio:{type:String}
+},{
+    timestamps:true
+});
+
+
+const SuperAdmin = mongoose.model('SuperAdmin',userSchema);
+
+module.exports = SuperAdmin;
