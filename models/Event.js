41c7b11e09feac6cb50d6eba66cb04c4b73c51b8
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-06-08 12:52:28 +0530

    merging complete as of now

--- models/Event.js
+++ models/Event.js
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const Users = require('./Users');
-
-const eventSchema = new Schema({
-    name:{
-        type: String,
-        required: true,
-        trim: true
-    },
-    venue:{type:String},
-    date : {type: Date,required : true},
-    description:{type: String,required: true},
-    poster_url:{type: String,required: true},
-    owner:{
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: 'Users'
-    },
-    participants : [String],
-    categories : {type:String,required:true},       // workshop, competiotion, talk-show
-    speaker : {type:String},
-    event_summary : {
-        gallery : [String],                           // will contain url of the uploaded images in simple array
-        chief_guest : {type:String},
-        award_winners : {type:String},
-        summary : {type:String},
-        outside_links : [String],
-        file_attachment : [String],
-        video_links : {type:String}
-    },
-    showcase_url : [String]
-},{
-    timestamps: true
-})
-
-const Event = mongoose.model('Events',eventSchema);
-
-module.exports = Event
-=======
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Users = require('./Users');
@@ -90,4 +49,3 @@ eventSchema.methods.filterByMonth = function(month) {
 const Event = mongoose.model('Events',eventSchema);
 
 module.exports = Event
->>>>>>> 891d8521a0cbf72cf3200f13caf8caec0962e1e5
