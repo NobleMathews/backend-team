45fd7f427ae06f0f7427664d21345f5a60592f24
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-06-12 18:39:05 +0530

    changes made in Club schema and routes changed accordingly

--- models/Club.model.js
+++ models/Club.model.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const clubSchema = new Schema({
     name:{type:String,required:true},
-    head:{type:String},
+    head:{type:Schema.Types.ObjectId,required:true,ref:'Users'},
     description:{type:String},
-    logo:{type:String}
+    logo_url:{type:String}
 })
 
 const Club = mongoose.model('club',clubSchema)
