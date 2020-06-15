45eb630c0ee78cc71f87bfc85273f14cae10695c
AASHRITH1903 <63637200+AASHRITH1903@users.noreply.github.com>
2020-06-15 19:54:22 +0530

    created routes and view to update achievement

--- server.js
+++ server.js
@@ -250,22 +250,6 @@ app.post('/admin/achievement/create/',upload.any('snapshot_url',20),(req,res)=>{
 })
 
 
-//to create readstream for achievement pics
-app.get('/achievement_pics/:filename', (req, res) => {
-  const file = gfs
-    .find({
-      filename: req.params.filename
-    })
-    .toArray((err, files) => {
-      if (!files || files.length === 0) {
-        return res.status(404).json({
-          err: 'no files exist'
-        })
-      }
-      gfs.openDownloadStreamByName(req.params.filename).pipe(res)
-    })
-  })
-
 // this route handle project creation currently i have set a arbitrary maximum of 20 images simultaneously
 // change as per necessity
 app.post('/admin/project/create', upload.any('snapshot_url',20), function (req, res, next) {
@@ -326,4 +310,28 @@ app.post('/admin/club/create', upload.single('logo'), function (req, res) { // t
         })
         res.redirect('/admin/clubs/retrieve')
       })
+    })
+
+    app.post('/admin/achievement/update/:id', upload.any('pics',20) ,(req, res) => { // for updating the achievement of a given id
+      const id = req.params.id
+      var pics_url;
+      if(req.files != undefined){
+          pics_url = req.files.map((file)=>{
+          return file.filename
+        })
+      }
+
+      var achievement = {
+        title: req.body.title,
+        caption: req.body.caption,
+        description: req.body.des,
+        pics_url: pics_url
+      }
+
+      Acheievement.findByIdAndUpdate(id, achievement)
+        .then(() => {
+          res.status(200).send('Achievement updated successfully')
+        }).catch(err => {
+          res.status(400).send(err)
+        })
     })
