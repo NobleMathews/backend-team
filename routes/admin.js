90d768ba8a802be9b9354882ae48f90c5ce831d5
arpit1912 <arpitbandejiya@gmail.com>
2020-07-12 13:58:36 +0530

    some change

--- routes/admin.js
+++ routes/admin.js
@@ -4,10 +4,9 @@ const superAdminModel = require('../models/SuperAdmin.model')
 const cookieParser = require('cookie-parser')
 const adminAuth = require('../middleware/adminAuth')
 
-
 // for rendering password page
 router.route('/password/change').get(adminAuth, (req, res) => {
-  res.render('update_password_admin', {page_name:"home"})
+  res.render('update_password_admin', { page_name: 'home' })
 })
 
 // password changing route
@@ -20,71 +19,66 @@ router.route('/password/change/').post(adminAuth, async (req, res) => {
   //     res.json(err)
   //   })
 
-  try{
-
+  try {
     req.admin.pswd = pswd
     await req.admin.save()
-    
+
     res.redirect(307, '/admin/profile')
-  }catch(e){
+  } catch (e) {
     res.json(e)
   }
 })
 
-//viewing the profile after logging in
+// viewing the profile after logging in
 router.route('/profile').post(adminAuth, (req, res) => {
-    //console.log('already logged in')
-    admins = {
-          _id: req.admin._id,
-          name: req.admin.name,
-          user_id: req.admin.user_id,
-          email_id: req.admin.email_id,
-          contact: req.admin.contact
-        }
-    
-    return res.render('landing_admin', { admin: admins, page_name:"home" })
+  // console.log('already logged in')
+  admins = {
+    _id: req.admin._id,
+    name: req.admin.name,
+    user_id: req.admin.user_id,
+    email_id: req.admin.email_id,
+    contact: req.admin.contact
+  }
+
+  return res.render('landing_admin', { admin: admins, page_name: 'home' })
 })
 
-//login to admin  
+// login to admin
 router.route('/login').post(async (req, res) => {
   const user_id = req.body.user_id
   const pswd = req.body.pswd
-  try{
-    //const admin = await superAdminModel.findByCredentials( user_id, pswd )
+  try {
+    // const admin = await superAdminModel.findByCredentials( user_id, pswd )
     const admin = await superAdminModel.findByCredentials(user_id, pswd)
 
-    if(!admin){
+    if (!admin) {
       throw new Error()
     }
 
     const token = await admin.generateAuthToken(req, res)
-    res.render('landing_admin', { admin: admin, page_name:"home" })
-
-  }catch(e){
+    res.render('landing_admin', { admin: admin, page_name: 'home' })
+  } catch (e) {
     // res.json(e)
-    res.render('index',{alerts:"Sorry, you do not have admin privileges"})
+    res.render('index', { alerts: 'Sorry, you do not have admin privileges' })
   }
-    
 })
 
 // for rendering the form
 router.route('/profile/update').get(adminAuth, async (req, res) => {
+  try {
+    const admin = req.admin
 
-    try{
-      const admin = req.admin
-
-      res.render('update_profile_admin', { id: admin._id, page_name:"home", user_id: admin.user_id, name: admin.name, contact: admin.contact, email_id: admin.email_id })
-    }catch(e){
-      res.status(400).json(e)
-    }
+    res.render('update_profile_admin', { id: admin._id, page_name: 'home', user_id: admin.user_id, name: admin.name, contact: admin.contact, email_id: admin.email_id })
+  } catch (e) {
+    res.status(400).json(e)
+  }
 })
 
 // for updating through the form
 router.route('/profile/update').post(adminAuth, async (req, res) => {
-  
   const updates = Object.keys(req.body)
 
-  try{
+  try {
     const admin = req.admin
 
     updates.forEach((update) => {
@@ -92,9 +86,9 @@ router.route('/profile/update').post(adminAuth, async (req, res) => {
     })
 
     await admin.save()
-    
-    res.render('landing_admin', { admin: admin, page_name:"home" })
-  }catch(e){
+
+    res.render('landing_admin', { admin: admin, page_name: 'home' })
+  } catch (e) {
     res.status(400).json(e)
   }
 })
@@ -124,21 +118,18 @@ router.route('/club_head/reset/:id').get(adminAuth, (req, res) => {
     })
 })
 
-router.route('/logout').get(adminAuth, async (req,res) => {
-    try{
+router.route('/logout').get(adminAuth, async (req, res) => {
+  try {
+    req.admin.tokens = req.admin.tokens.filter((token) => {
+      return token.token !== req.token
+    })
 
-      req.admin.tokens = req.admin.tokens.filter((token) => {
-        return token.token !== req.token
-      })
+    await req.admin.save()
 
-      await req.admin.save()
-
-      res.redirect('/admin')
-
-    }catch(e){
-      res.json({e})
-    }
-    
+    res.redirect('/admin')
+  } catch (e) {
+    res.json({ e })
+  }
 })
 
 module.exports = router
