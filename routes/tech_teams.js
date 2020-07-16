a12570ae8fd3729351f6eb26001d341d6d447b90
NobleMathews <55821103+NobleMathews@users.noreply.github.com>
2020-07-17 00:47:05 +0530

    Route Logic
    Error fixes and basic requirements

--- routes/tech_teams.js
+++ routes/tech_teams.js
@@ -8,20 +8,28 @@ router.route('/create').get(adminAuth,(req, res) => {
   res.render('create_tech_team',{page_name:"tech_teams"})
 })
 
-router.route('/create/').post(adminAuth, (req, res) => {
+router.route('/create/').post(adminAuth, uploadf.fields([{name:'team_poster_url',maxCount:1},{name:'dp_url',maxCount:1}]), (req, res) => {
+  var dp_url="",team_poster_url="";
+  if (req.files != undefined) {
+  if(req.files['team_poster_url'])
+  team_poster_url=req.files['team_poster_url'][0].filename;
+  if(req.files['dp_url'])
+  dp_url=req.files['dp_url'][0].filename;
+  }
   var TechTeam = new techTeamModel({
     team_name: req.body.team_name,
     tech_head: req.body.tech_head,
     contact: req.body.contact,
-    dp_url: req.body.dp_url,
+    dp_url: dp_url,
     email_id: req.body.email_id,
     ref_link: req.body.ref_link,
     description: req.body.description,
-    team_poster_url: req.body.team_poster_url
+    team_poster_url: team_poster_url
   })
   TechTeam.save((err, club) => {
     if (err) { req.flash('error', err.message) }
-  }).then(res.redirect('/admin/'))
+    res.redirect('/tech_teams/view_all')
+  })
 })
 
 router.route('/update/:id').get(adminAuth,(req, res) => {
@@ -68,7 +76,7 @@ router.route('/view_all').get(adminAuth, (req, res) => {
           req.flash("error",err.message)
           res.redirect('/tech_teams/view_all')
       }else{
-          res.render('view_tech_teams',{tech_teams:tech_teams})
+          res.render('view_tech_teams',{tech_teams:tech_teams,alerts: req.flash('error'), page_name:"tech_teams"})
       }
   })
 })
