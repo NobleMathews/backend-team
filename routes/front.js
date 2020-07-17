e2a8871a336a0eb36906da85329d86f10ca34f61
shobhi1310 <42507632+shobhi1310@users.noreply.github.com>
2020-07-17 18:44:53 +0530

    register route shifted to fron

--- routes/front.js
+++ routes/front.js
@@ -169,5 +169,31 @@ router.route('/challenges').get((req, res) => {
         }
     });
 })
+// route for registering pusposes to an event, takes emailid as input parameter in body
+router.route('register/:id').post((req,res)=>{
+  const id = req.params.id
+  const email = req.body.email
+  
+  eventsModel.findById(id)
+  .then(event => {
+
+  let i=0;
+  for(i=0;i<event.participants.length;i++){
+      if(event.participants[i] === email){
+          break;
+      }
+  }
+
+  if(i === event.participants.length){
+      event.participants.push(email);
+      event.save();
+      res.status(200).json("Subscribed");
+  }else{
+      res.status(200).json("Email already exists");
+  }
+  }).catch(err => {
+      res.status(404).json(err);
+  })
+})
 
 module.exports = router;
