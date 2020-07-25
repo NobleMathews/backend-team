770f654a9ad45ab1225a7c987903ee0543c63b61
unknown <CS18B008@iittp.ac.in>
2020-07-26 03:35:48 +0530

    Routes under projects OAuth authorized Public creator mail added to creator of projects

--- models/Public.model.js
+++ models/Public.model.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const { TokenExpiredError } = require('jsonwebtoken');
 const Schema = mongoose.Schema;
 
+//Creator as a public
 const PublicSchema = new Schema({
     googleId:{
         type: String,
