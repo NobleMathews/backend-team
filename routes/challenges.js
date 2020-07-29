6263e141671f080f8a61d589088b396fe7fb9f40
NobleMathews <55821103+NobleMathews@users.noreply.github.com>
2020-07-29 09:40:32 +0530

    Update edits

--- routes/challenges.js
+++ routes/challenges.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const challengeModel = require('../models/Challenge.model')
 const {upload, uploadf}= require('../db/upload')
 const adminAuth = require('../middleware/adminAuth');
+const mongoose = require('mongoose')
 
 router.route('/create/').get(adminAuth, (req, res) => {
     res.render('create_challenge', { alerts: req.flash('error'),page_name:"challenge"})
@@ -26,7 +27,7 @@ router.route('/create').post(adminAuth,upload.single('photo'), (req, res) => {
         ref_url:req.body.ref_url,
         photo: photo_url,
         category: req.body.category,
-        documentIDs:documentIDs,
+        documentIDs:documentIDs
       })
     
       challenge.save((err, challenge) => { // saving the challenge in database
@@ -37,13 +38,85 @@ router.route('/create').post(adminAuth,upload.single('photo'), (req, res) => {
       }) 
 })
 
-router.route('/update').get(adminAuth, async (req, res) => {
-    
+router.route('/update/:id').get(adminAuth, async (req, res) => {
+    const id = req.params.id
+    challengesModel.findById(id)
+    .then(challenge=>{
+        res.render('update_challenge', { alerts: req.flash('error'),challenge:challenge,moment:moment, page_name:'challenge' })
+    }).catch(err=>{
+        req.flash("error",err.message)
+        res.redirect('/challenges/view_all')
+    })
 })
 
 
-router.route('/update/').post(adminAuth, upload.single('photo') ,async (req, res) => {
-    
+router.route('/update/:id').post(adminAuth, upload.single('photo') ,async (req, res) => {
+    const id = req.params.id;
+    var ev,documentIDs=[],deletequeue=[],pics_url=[],masterqueue=[];
+    if(req.body.documentIDs){
+      documentIDs = JSON.parse(req.body.documentIDs); 
+    }
+    if (req.file == undefined) {
+        ev={
+            name: req.body.name,
+            description: req.body.description,
+            registration_end: req.body.registration_end,
+            registration_start: req.body.registration_start,
+            ref_url:req.body.ref_url,
+            category: req.body.category,
+            documentIDs:documentIDs
+        }
+    } else {
+        // req.files.forEach(function (file,index) {
+          masterqueue.push([req.file.filename,req.file.id]);
+        // })
+        pics_url.push(req.file.filename)
+        masterqueue=masterqueue.concat(documentIDs);
+        documentIDs =masterqueue.filter(k => pics_url.includes(k[0])); 
+        deletequeue = masterqueue.filter(k =>!pics_url.includes(k[0]));
+        ev={
+            name: req.body.name,
+            description: req.body.description,
+            registration_end: req.body.registration_end,
+            registration_start: req.body.registration_start,
+            ref_url:req.body.ref_url,
+            category: req.body.category,
+            documentIDs:documentIDs,
+            photo:`${req.file.filename}`,
+      }
+    }
+
+    challengesModel.findOne({_id: id},function(err,challenge){
+      if(err) {
+        req.flash("error",err.message)
+        return res.redirect('/challenges/view_all')
+      }
+        for (var id in ev ){
+          challenge[id]= ev[id];
+        }
+        challenge.save(function(err){
+          if (err) {
+          req.flash("error",err.message)
+          }
+          if(deletequeue.length>0){
+            var arrPromises = deletequeue.map((path) => 
+            {if (req.app.locals.gfs) {
+              req.app.locals.gfs.delete(new mongoose.Types.ObjectId(path[1]))
+              }
+            }
+            );
+            Promise.all(arrPromises)
+              .then((arrdata) => {res.redirect('/challenges/view_all')})
+              .catch(function (err) {
+                req.flash("error",["Alert : Delete failed on some images."])
+                res.redirect('/challenges/view_all')
+              });
+          }
+          else{
+            res.redirect('/challenges/view_all')
+          }        
+        })
+    });
 })
 
 router.route('/delete/:id').delete(adminAuth, (req, res) => { 
