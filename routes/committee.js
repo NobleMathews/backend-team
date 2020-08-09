1c7e9d634a8e563920fbe8fa295ba156a077fb48
NobleMathews <55821103+NobleMathews@users.noreply.github.com>
2020-08-10 00:18:36 +0530

    Minor Committee changes

--- routes/committee.js
+++ routes/committee.js
@@ -29,7 +29,7 @@ router.route('/create/').post(adminAuth, upload.single('dp_url'), async (req, re
       members: member
     })
     // console.log(committee)
-    committeemodel.updateOne({ _id: '5f149d11bad71e2d6c7bcb46' }, { $push: { members: member } }, (err, user) => {
+    committeemodel.findOneAndUpdate({}, { $push: { members: member } }, (err, user) => {
       if (err) {
         req.flash('error', err.message)
         res.redirect('/committee_members/view_all')
@@ -46,12 +46,12 @@ router.route('/create/').post(adminAuth, upload.single('dp_url'), async (req, re
 router.route('/update/:id').get(adminAuth, async (req, res) => {
   const member_id = req.params.id
   try {
-    committeemodel.findOne({ _id: '5f149d11bad71e2d6c7bcb46' }, (err, member) => {
+    committeemodel.findOne({'members._id': member_id }, (err, committee) => {
       if (err) {
         req.flash('error', err.message)
         return res.redirect('/committee_members/view_all')
       }
-      member_details = member.members.find(o => { return o._id == member_id })
+      member_details = committee.members.find(o => { return o._id == member_id })
       res.render('update_committee_member', { alerts: req.flash('error'), member: member_details, page_name: 'committee_members' })
     })
   } catch (err) {
@@ -79,7 +79,7 @@ router.route('/update/:id').post(adminAuth, upload.single('dp'), async (req, res
     })
     member.dp_url = dpurl
 
-    await committeemodel.findOneAndUpdate({ _id: '5f149d11bad71e2d6c7bcb46', 'members._id': member_id }, { $set: { 'members.$': member } })
+    await committeemodel.findOneAndUpdate({'members._id': member_id }, { $set: { 'members.$': member } })
 
     res.redirect('/committee_members/view_all')
   } catch (err) {
@@ -90,7 +90,7 @@ router.route('/update/:id').post(adminAuth, upload.single('dp'), async (req, res
 router.route('/delete/:id').get(adminAuth, async (req, res) => {
   committeemodel
     .updateOne(
-      { _id: '5f149d11bad71e2d6c7bcb46' },
+      {'members._id': member_id },
       { $pull: { members: { _id: [req.params.id] } } }, { safe: true }, function (err, user) {
         if (err) {
           req.flash('error', err.message)
@@ -105,8 +105,9 @@ router.route('/delete/:id').get(adminAuth, async (req, res) => {
 router.route('/view_all').get(adminAuth, async (req, res) => {
   let committee_members
   try {
-    committee_members = await committeemodel.findOne({ _id: '5f149d11bad71e2d6c7bcb46' })
-    if (committee_members) { res.render('view_committee_members', { alerts: req.flash('error'), members: committee_members.members, page_name: 'committee_members' }) } else { res.render('view_committee_members', { alerts: req.flash('error'), members: [], page_name: 'committee_members' }) }
+    committee_members = await committeemodel.findOne({})
+    if (committee_members) { res.render('view_committee_members', { alerts: req.flash('error'), members: committee_members.members, page_name: 'committee_members' }) } 
+    else { res.render('view_committee_members', { alerts: req.flash('error'), members: [], page_name: 'committee_members' }) }
   } catch (error) {
     req.flash('error', err.message)
     res.redirect('/admin/login/')
